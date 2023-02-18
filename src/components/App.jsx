8d1bb5ea7829863179afeb3024a04104c5645874
ilcommitfa <110323544+ilcommitfa@users.noreply.github.com>
2023-02-18 18:18:03 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
-export const App = () => {
+import { useState } from 'react';
+import Statistics from './Statistics';
+import FeedbackOptions from './FeedbackOptions';
+import Section from './Section';
+import Notification from './Notification';
+
+function App() {
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const handleFeedback = option => {
+    setFeedback(prevState => ({ ...prevState, [option]: prevState[option] + 1 }));
+  };
+
+  const countTotalFeedback = () => {
+    return Object.values(feedback).reduce((total, value) => total + value, 0);
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    const positive = feedback.good;
+    return total > 0 ? Math.round((positive / total) * 100) : 0;
+  };
+
+  const options = Object.keys(feedback);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Section title="Please leave your feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={handleFeedback} />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
     </div>
   );
-};
+}
+
+export default App;
---
+++ src/components/FeedbackOptions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <>
+    {options.map(option => (
+      <button key={option} onClick={() => onLeaveFeedback(option)}>
+        {option}
+      </button>
+    ))}
+  </>
+);
+
+export default FeedbackOptions;
